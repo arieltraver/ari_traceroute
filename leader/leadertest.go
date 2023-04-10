9808d64b0db25b570f5ff88e40ae92125572d705
arieltraver <arieltraver@users.noreply.github.com>
2023-04-10 14:20:21 -0400

    work on test

--- leader/leadertest.go
+++ leader/leadertest.go
@@ -4,6 +4,7 @@ import (
 	"net/rpc"
 	"log"
 	"time"
+	"github.com/arieltraver/ari_traceroute/set"
 )
 
 func dialLeader(address string) *rpc.Client {
@@ -13,15 +14,31 @@ func dialLeader(address string) *rpc.Client {
 		for {
 			select {
 			case <- connectTimer.C:
-				fmt.Println("failed to connect within time limit")
+				log.Println("failed to connect within time limit")
 				return nil
 			default:
 				client, err = rpc.DialHTTP("tcp", address)
 				if err == nil {
 					return client
+				} else {
+					log.Println(err)
 				}
 			}
 		}
 	}
 	return client
+}
+
+func getIpRange(leader *rpc.Client) {
+	arguments := IpArgs {
+		probeId:"test",
+	}
+	reply := IpReply{}
+	err := leader.Call("RaftNode.RequestVote", arguments, &reply)
+	if err != nil {
+		return
+	}
+	for _, ip := range(reply.ips) {
+		fmt.Println(ip)
+	}
 }
